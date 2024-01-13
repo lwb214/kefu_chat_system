14502fe030e4bb064a8214b87ebb80239ba98692
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2024-01-13 19:54:05 +0800

    项目初始搭建

---
+++ src/main/java/com/kefu/KefuChatSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kefu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KefuChatSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KefuChatSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kefu/KefuChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KefuChatSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
